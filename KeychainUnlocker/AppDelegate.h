82b013aac25e41f7993adbb4d06a48568ddd10cb
Rasmus Sten <rasmus@dll.nu>
2014-10-04 19:32:46 +0300

    Initial Commit

---
+++ KeychainUnlocker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KeychainUnlocker
+//
+//  Created by Rasmus Sten on 04-10-2014.
+//  Copyright (c) 2014 Rasmus Sten. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ KeychainUnlocker/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  KeychainUnlocker
+//
+//  Created by Rasmus Sten on 04-10-2014.
+//  Copyright (c) 2014 Rasmus Sten. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
